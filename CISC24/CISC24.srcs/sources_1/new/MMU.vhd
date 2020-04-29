aaa6f2549aac622061c79573c6a28e92a0776709
Bradpaiva <bpaiva1@umassd.edu>
2020-04-29 12:18:16 -0400

    errors in last commit

--- CISC24/CISC24.srcs/sources_1/new/MMU.vhd
+++ CISC24/CISC24.srcs/sources_1/new/MMU.vhd
@@ -32,21 +32,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity MMU is
-<<<<<<< HEAD
-  Port (clk        : in Std_logic;   
-        enable     : in std_logic; 	
-        reset      : in std_logic;
-        address_in : in std_logic_vector(15 downto 0); --input addresses for memory fetch
-        address_out: out std_logic_vector(7 downto 0); --output memory address
-        data_in    : in std_logic_vector(7 downto 0); 
-  --ram_address: in std_logic_vector(19 downto 0);
-  --IMM_OOP	 : in STD_LOGIC_VECTOR(23 downto 0);
-  --IMM_TOP	 : in STD_LOGIC_VECTOR(23 downto 0);
-  --IMM_MEM    : in STD_LOGIC_VECTOR(23 downto 0);
-  --IMM_BRN    : in STD_LOGIC_VECTOR(23 downto 0);
-  --MSK		 : in STD_LOGIC_VECTOR(3  downto 0);
-
-=======
   Port (clk        :  in Std_logic;   
         enable     : in std_logic; 	
         reset      : in std_logic;
@@ -54,14 +39,9 @@ entity MMU is
         address_out: out std_logic_vector(9 downto 0);
         data_in    : in std_logic_vector(23 downto 0); --output memory address
         data_out    : out std_logic_vector(23 downto 0)); 
->>>>>>> adeead18032e46e211b4606e8a7abee04a541fa7
 end MMU;
 
 architecture Behavioral of MMU is
-<<<<<<< HEAD
-component 
-
-=======
 component Memory is
     Port ( clk : in std_logic;
            we : in std_logic;
@@ -70,7 +50,6 @@ component Memory is
            dataout : out std_logic_vector(23 downto 0) 
             );
 end component;
->>>>>>> adeead18032e46e211b4606e8a7abee04a541fa7
 begin
 
 C1: Memory port map (clk => clk,
